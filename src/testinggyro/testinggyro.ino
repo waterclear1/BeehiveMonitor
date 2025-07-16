d9fd5b0e0f000fba4148747fa059dfb0b5aae5a7
waterclear1 <23110189@st.vju.ac.vn>
2025-07-16 17:15:24 +0700

    adding docs

--- src/testinggyro/testinggyro.ino
+++ src/testinggyro/testinggyro.ino
@@ -1,54 +1,74 @@
-/*
- ADXL362_SimpleRead.ino -  Simple XYZ axis reading example
- for Analog Devices ADXL362 - Micropower 3-axis accelerometer
- go to http://www.analog.com/ADXL362 for datasheet
- 
- 
- License: CC BY-SA 3.0: Creative Commons Share-alike 3.0. Feel free 
- to use and abuse this code however you'd like. If you find it useful
- please attribute, and SHARE-ALIKE!
- 
- Created June 2012
- by Anne Mahaffey - hosted on http://annem.github.com/ADXL362
-
- Modified May 2013
- by Jonathan Ruiz de Garibay
- 
-Connect SCLK, MISO, MOSI, and CSB of ADXL362 to
-SCLK, MISO, MOSI, and DP 10 of Arduino 
-(check http://arduino.cc/en/Reference/SPI for details)
- 
-*/ 
+// Standalone sketch for ADXL362, configured for high-sensitivity beehive monitoring.
+// This version is specifically for the provided "annem/ADXL362" library files.
 
 #include <SPI.h>
-#include <ADXL362.h>
+#include "ADXL362.h" // Using your provided library files
 
+// Define the Chip Select pin for your ADXL362
+#define SPI_CS_ADXL 10
+#define SPI_MOSI 11
+#define SPI_MISO 12
+#define SPI_SCK 13
+// Create an instance of the sensor library
 ADXL362 xl;
 
-int16_t temp;
-int16_t XValue, YValue, ZValue, Temperature;
+void setup() {
+    // 1. Start Serial Communication
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println("High-Sensitivity Beehive Monitor Startup...");
 
-void setup(){
-  
-  Serial.begin(115200);
-  xl.begin(10);                   // Setup SPI protocol, issue device soft reset
-  xl.beginMeasure();              // Switch ADXL362 to measure mode  
-	
-  Serial.println("Start Demo: Simple Read");
+    // 2. Initialize the ADXL362 using its begin() function
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS_ADXL);
+    xl.begin(SPI_CS_ADXL);  // Setup SPI protocol, issue device soft reset
+    xl.beginMeasure();      // Switch ADXL362 to measure mode
+   Serial.println("ADXL362 initialized.");
+    // --- MANUALLY CONFIGURE SENSOR REGISTERS ---
+    // We do this because this library version lacks high-level functions like setRange().
+
+    // Configure FILTER_CTL Register (Address 0x2C)
+    // Set Range to +/- 2g, Bandwidth to ODR/4, and ODR to 100Hz
+    // Value = 0b00010011 = 0x13
+    Serial.println("Setting Range, ODR, and Filter...");
+    xl.SPIwriteOneRegister(0x2C, 0x13);
+    delay(10); // Small delay after writing
+
+    // Configure POWER_CTL Register (Address 0x2D)
+    // Set to Low Noise Mode and Measurement Mode
+    // Value = 0b00010010 = 0x12
+    Serial.println("Setting Low Noise and Measurement Mode...");
+    xl.SPIwriteOneRegister(0x2D, 0x12);
+    delay(10); // Small delay after writing
+
+
+    // NOTE: We do NOT call xl.beginMeasure() anymore, because our write
+    // to the POWER_CTL register already put the device in measurement mode.
+
+    Serial.println("\nConfiguration complete. Starting measurements...");
 }
 
-void loop(){
+void loop() {
+    // Reading data every second for this example
+    delay(1000); 
+
+    int16_t x, y, z, temp;
     
-  // read all three axis in burst to ensure all measurements correspond to same sample time
-  xl.readXYZTData(XValue, YValue, ZValue, Temperature);  
-  Serial.print("XVALUE=");
-  Serial.print(XValue);	 
-  Serial.print("\tYVALUE=");
-  Serial.print(YValue);	 
-  Serial.print("\tZVALUE=");
-  Serial.print(ZValue);	 
-  Serial.print("\tTEMPERATURE=");
-  Serial.println(Temperature);	 
-  delay(100);                // Arbitrary delay to make serial monitor easier to observe
-}
+    // Read all three axes and temperature data
+  // Read ADXL362 data.
+  xl.readXYZTData(x, y, z, temp);
 
+  // To use the Serial Plotter, print each value separated by a tab ("\t").
+  // Use println() only on the very last value to signal a new data set for the plot.
+  Serial.print(x);
+  Serial.print("\t");
+  Serial.print(y);
+  Serial.print("\t");
+  Serial.println(z); // This plots X, Y, and Z as three separate lines.
+
+  // The printValues() function is commented out because its text-based
+  // output would interfere with the Serial Plotter's graph.
+  // printValues();
+
+  // A shorter delay provides a more responsive plot.
+  delay(200);
+}
--- src/testingsensors/testingsensors.ino
+++ src/testingsensors/testingsensors.ino
@@ -1,5 +1,3 @@
-#include <WiFi.h>
-#include <WebServer.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
@@ -24,12 +22,6 @@ ADXL362 xl;
 float bme_temperature, bme_humidity, bme_pressure, bme_altitude;
 int16_t adxl_x, adxl_y, adxl_z, adxl_temperature;
 
-// WiFi credentials
-const char* ssid = "aaa";  // Enter SSID here
-const char* password = "123";  // Enter Password here
-
-WebServer server(80);
-
 void setup() {
   Serial.begin(115200);
   delay(100);
@@ -39,35 +31,19 @@ void setup() {
   if (!bme.begin(0x76, &Wire)) {
     Serial.println("BME280 not found!");
     while (1);
+    
+  } else {
+    Serial.println("BME280 initialized");
   }
 
   // Initialize SPI for ADXL362
   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS_ADXL);
   xl.begin(SPI_CS_ADXL);  // Setup SPI protocol, issue device soft reset
-  xl.beginMeasure();      // Switch ADXL362 to measure mode
+  xl.beginMeasure();       // Switch ADXL362 to measure mode
   Serial.println("ADXL362 initialized.");
-
-  // Connect to WiFi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: "); Serial.println(WiFi.localIP());
-
-  // Setup web server routes
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-  server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop() {
-  server.handleClient();
 
   // Read ADXL362 data for serial output
   xl.readXYZTData(adxl_x, adxl_y, adxl_z, adxl_temperature);
@@ -75,49 +51,32 @@ void loop() {
   Serial.print("\tYVALUE="); Serial.print(adxl_y);
   Serial.print("\tZVALUE="); Serial.print(adxl_z);
   Serial.print("\tTEMPERATURE="); Serial.println(adxl_temperature);
-  delay(100);  // Short delay for serial readability
+  
+  printValues();
+  delay(2000);  // Short delay for serial readability
 }
 
-void handle_OnConnect() {
-  // Read BME280 data
-  bme_temperature = bme.readTemperature();
-  bme_humidity = bme.readHumidity();
-  bme_pressure = bme.readPressure() / 100.0F;
-  bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
+  
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
 
-  // Read ADXL362 data for web display
-  xl.readXYZTData(adxl_x, adxl_y, adxl_z, adxl_temperature);
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
-  // Send HTML response
-  server.send(200, "text/html", SendHTML(bme_temperature, bme_humidity, bme_pressure, bme_altitude, adxl_x, adxl_y, adxl_z, adxl_temperature));
-}
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
 
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float temp, float hum, float press, float alt, int16_t x, int16_t y, int16_t z, int16_t temp_adxl) {
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>Beehive Monitor</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<div id=\"webpage\">\n";
-  ptr += "<h1>Beehive Monitor</h1>\n";
-  ptr += "<p>Temperature (BME280): "; ptr += temp; ptr += "&deg;C</p>";
-  ptr += "<p>Humidity (BME280): "; ptr += hum; ptr += "%</p>";
-  ptr += "<p>Pressure (BME280): "; ptr += press; ptr += "hPa</p>";
-  ptr += "<p>Altitude (BME280): "; ptr += alt; ptr += "m</p>";
-  ptr += "<p>X-Axis (ADXL362): "; ptr += x; ptr += "</p>";
-  ptr += "<p>Y-Axis (ADXL362): "; ptr += y; ptr += "</p>";
-  ptr += "<p>Z-Axis (ADXL362): "; ptr += z; ptr += "</p>";
-  ptr += "<p>Temperature (ADXL362): "; ptr += temp_adxl; ptr += "</p>";
-  ptr += "</div>\n";
-  ptr += "</body>\n";
-  ptr += "</html>\n";
-  return ptr;
+  Serial.println();
 }
